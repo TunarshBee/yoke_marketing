71df22f3343b8225ab718ae3ace758fc269fe205
TunarshBee <rafiubello4real@gmail.com>
2023-03-05 17:33:49 +0100

    Created the lists of cities

---
+++ src/City.ts
@@ -0,0 +1,23 @@
+interface City {
+  name: string;
+  latitude: number;
+  longitude: number;
+}
+export const cities: City[] = [
+  {
+    name: "New York",
+    latitude: 40.712776,
+    longitude: -74.005974,
+  },
+  {
+    name: "London",
+    latitude: 51.507351,
+    longitude: -0.127758,
+  },
+  {
+    name: "Paris",
+    latitude: 48.856613,
+    longitude: 2.352222,
+  },
+  // Add more cities here
+];
